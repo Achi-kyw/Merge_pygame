90f3c22c698b8e473115c3a49b4859a6fb968f9d
Achi-kyw <58551816+Achi-kyw@users.noreply.github.com>
2023-07-05 18:17:03 +0800

    好多東西弄好了XD

--- object.py
+++ object.py
@@ -14,19 +14,17 @@ class pinxiang():
     def draw(self,screen):
         if not self.display:
             return
-        if self.selected:
-            pg.draw.rect(screen,(255,0,0),[self.x,self.y,self.width,self.height],3)
-        else:
-            pg.draw.rect(screen,(0,0,0),[self.x,self.y,self.width,self.height],3)
+        screen.blit(pg.image.load('image/pinxiang_100.jpg'), (self.x,self.y))
+        pg.draw.rect(screen,(255*int(self.selected),0,0),[self.x,self.y,self.width,self.height],3)
         if self.weight<10:
             font = pg.font.SysFont("couriernew", 50)
-            text = font.render(str(self.weight), True, (0,0,0), (255,255,255))
-            screen.blit(text, (self.x+35,self.y+25))
+            text = font.render(str(self.weight), True, (255*int(self.selected),0,0), (255,255,255))
+            screen.blit(text, (self.x+35,self.y+105))
         elif self.weight<100:
             font = pg.font.SysFont("couriernew", 50)
-            text = font.render(str(self.weight), True, (0,0,0), (255,255,255))
-            screen.blit(text, (self.x+20,self.y+25))
+            text = font.render(str(self.weight), True, (255*int(self.selected),0,0), (255,255,255))
+            screen.blit(text, (self.x+20,self.y+105))
         else:
             font = pg.font.SysFont("couriernew", 40)
-            text = font.render(str(self.weight), True, (0,0,0), (255,255,255))
-            screen.blit(text, (self.x+15,self.y+30))
+            text = font.render(str(self.weight), True, (255*int(self.selected),0,0), (255,255,255))
+            screen.blit(text, (self.x+15,self.y+110))
